e23ad60b35c131b43b570e9a6f301026ef7e68b9
Mark Lott <mlott@mlott-ltm5.internal.salesforce.com>
2020-02-25 20:57:53 -0600

    prettied

--- lwc/client/index.js
+++ lwc/client/index.js
@@ -1,4 +1,4 @@
-import { buildCustomElementConstructor } from 'lwc';
-import MyApp from 'my/app';
+import { buildCustomElementConstructor } from "lwc";
+import MyApp from "my/app";
 
-customElements.define('my-app', buildCustomElementConstructor(MyApp));
+customElements.define("my-app", buildCustomElementConstructor(MyApp));
