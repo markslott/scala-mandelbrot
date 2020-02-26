3a6caee0a681ab00aa0f52e0d57eb27457a628ef
Mark Lott <mlott@mlott-ltm5.internal.salesforce.com>
2020-02-25 21:51:34 -0600

    made recursive

--- app/services/Mandelbrot.scala
+++ app/services/Mandelbrot.scala
@@ -5,28 +5,27 @@ import play.api.libs.json._
 
 class Mandelbrot {
 
-    val escaperadius = 2; //must be set to at least 2
+    val escaperadius = 4; //must be set to at least 2
 
     def squared(x:Double) = x*x;
 
     //where the magic happens - is a given complex number a member of the mandelbrot set
     //or not? The number of iterations it takes escape (or not) determines the color
     //using a smoothing algorithm to make the color transitons look smooth
-    def compute(z0 : Complex, depth : Long) : Long =  {
+    def compute(c : Complex, depth : Long) : Long =  {
         
-        def mandelbrot = (z0: Complex) => {
-            var (i,z) = (0,z0.copy());
-            while ((z.abs < escaperadius) && (i < depth)) {
-                z = z * z + z0;
-                i += 1;
-            } 
-            (z,i,i >= depth)
+        def mandelbrot = (c: Complex) => {
+            def accumulator (c : Complex, z : Complex = new Complex(0,0), n : Long = 0) : (Complex,Long,Boolean) = n match {
+                case 0 => accumulator(c,c,1)
+                case _ => if ((z.abs > escaperadius) || (n >= depth)) (z,n, n>=depth) else accumulator(c,z*z+c,n+1)
+            }
+            accumulator(c)
          }
-        val (finalZ,iterations,didNotEscape) = mandelbrot(z0);
-        def smoothValue(i:Long,x:Double) = i - Math.log(Math.log(x))/Math.log(2);
-        val mu = smoothValue(iterations,squared(finalZ.abs));
-        val color = if (didNotEscape) 0.0 else if (mu < 0) 0.0 else mu;
-        color.toInt;
+        val (finalZ,iterations,didNotEscape) = mandelbrot(c)
+        def smoothValue(i:Long,x:Double) = i - Math.log(Math.log(x))/Math.log(2)
+        val mu = smoothValue(iterations,squared(finalZ.abs))
+        val color = if (didNotEscape) 0.0 else if (mu < 0) 0.0 else mu
+        color.toInt
     }
 
     //takes a square grid and computes the color for each individual point. Returns a two 
@@ -36,11 +35,11 @@ class Mandelbrot {
     //and only sample 144 points in the defined square. Depth is how far down to look - deeper zoom
     //levels will need larger depth values to see if that point escapes or not.
     def draw(start : Complex, distance : Double, resolution : Integer, depth : Long) : JsValue = {
-        var grid = Array.ofDim[Long](resolution,resolution);
+        var grid = Array.ofDim[Long](resolution,resolution)
         for (y <- 0 until resolution; x <- 0 until resolution) {
-            grid(x)(y) = compute(Complex(start.r + distance * x / resolution ,start.i - distance * y / resolution), depth);
+            grid(x)(y) = compute(Complex(start.r + distance * x / resolution ,start.i - distance * y / resolution), depth)
         }
-        Json.toJson(grid);
+        Json.toJson(grid)
     }
     
 }
